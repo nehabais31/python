44d2240769eecc2db9887b2b682b52578050216c
Neha Bais <60439971+nehabais31@users.noreply.github.com>
2020-05-12 10:10:25 -0400

    Delete convert_feet_to_meters.py

--- convert_feet_to_meters.py
+++
@@ -1,9 +0,0 @@
-"""
-Neha Bais
-
-Takes input from user in Feet & converts it in meters !!!
-"""
-
-feet = eval(input("Enter a number in Feet : "))
-meters = 0.305 * feet
-print (feet, "Feet is", meters , "in meters")
